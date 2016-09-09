91a46e77080ddff80d494152d04caf7931487232
Josue Cardona <josuecardonap@gmail.com>
2016-09-09 07:10:21 -0400

    .

--- src/app/city/city.module.ts
+++ src/app/city/city.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
 import { CityComponent } from './city.component';
-import { CityListComponent } from './city-list.component';
-import { CityDetailsComponent } from './city-details.component';
+// make separate plan folder?
+// import { CityListComponent } from './city-list.component';
+// import { CityDetailsComponent } from './city-details.component';
 import { CityService } from './city.service';
 import { routing } from './city.routing';
 
